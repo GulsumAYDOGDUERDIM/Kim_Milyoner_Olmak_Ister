8790ddbfaae63f804163837befa40ac9187717c4
AlpayCa <alpay41@gmail.com>
2023-03-30 02:22:49 +0300

    feat: Add rules

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace millionaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Kim Milyoner Olmak İster - V1
+            Console.WriteLine("Kim Milyoner Olmak İster\n");
+            Console.Write("İsim Giriniz: ");
+            string isim = Console.ReadLine();
+            Console.Write("Soyisim Giriniz: ");
+            string soyisim = Console.ReadLine();
+
+
+            Console.Write("\n1.Telefon Jokerinizi Giriniz: ");
+            string telefonJoker1 = Console.ReadLine();
+            Console.Write("2.Telefon Jokerinizi Giriniz: ");
+            string telefonJoker2 = Console.ReadLine();
+            Console.Write("3.Telefon Jokerinizi Giriniz: ");
+            string telefonJoker3 = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Hoşgeldiniz " + isim + " " + soyisim);
+
+            Console.Write("Kuralları Biliyor Musunuz? : ");
+            char kuralCevap = char.Parse(Console.ReadLine().ToUpper());
+
+            if (kuralCevap != 'E')
+            {
+                // TODO: Kurallar eklenecek.
+                Console.WriteLine("Kurallar: asdfasdaads");
+            }
+
+            Console.Write("Hazır Mısınız? : ");
+            char hazirCevap = char.Parse(Console.ReadLine().ToUpper());
+            #endregion
+
+        }
+    }
+}
