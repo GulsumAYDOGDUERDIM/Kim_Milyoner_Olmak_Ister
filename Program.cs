773e6882ce6bb3517159637c4afc3f2cabff1d1a
AlpayCa <alpay41@gmail.com>
2023-03-30 02:39:03 +0300

    feat: Added the right to wildcard selection

--- Program.cs
+++ Program.cs
@@ -86,6 +86,58 @@
             if (jokerHak) Console.Write(" Joker için J'ye,");
             Console.Write(" Çekilmek için R'ye Basınız: ");
             char secim = char.Parse(Console.ReadLine().ToUpper());
+
+            if (secim == 'J')
+            {
+                if (!jokerHak)
+                {
+                    Console.WriteLine("Joker Hakkınız Bitmiştir..");
+                    goto yanitNoktasi;
+                }
+
+                Console.WriteLine("1-) " + (seyirciHak ? "Seyirci" : ""));
+                Console.WriteLine("2-) " + (yuzdeHak ? "%50" : ""));
+                Console.WriteLine("3-) " + (telefonHak ? "Telefon" : ""));
+                Console.Write("Seçiminizi Yapınız: ");
+                int jokerCevap = int.Parse(Console.ReadLine());
+
+                if (jokerCevap == 1 && seyirciHak)
+                {
+                    Console.WriteLine("A) %" + seyirciYuzdeA);
+                    Console.WriteLine("B) %" + seyirciYuzdeB);
+                    Console.WriteLine("C) %" + seyirciYuzdeC);
+                    Console.WriteLine("D) %" + seyirciYuzdeD);
+                    seyirciHak = false;
+                }
+                else if (jokerCevap == 2 && yuzdeHak)
+                {
+                    // TODO: Bu kısım sorudan bağımsız kullanılabilmeli.
+                    gizleB = true;
+                    gizleC = true;
+
+                    yuzdeHak = false;
+                    goto soruNoktasi;
+                }
+                else if (jokerCevap == 3 && telefonHak)
+                {
+                    Console.WriteLine("1-) " + telefonJoker1);
+                    Console.WriteLine("2-) " + telefonJoker2);
+                    Console.WriteLine("3-) " + telefonJoker3);
+                    Console.Write("Kimi Aramak İstersiniz? : ");
+                    int telefonCevap = int.Parse(Console.ReadLine());
+
+                    if (soruNo <= 7 || telefonCevap == 3) Console.WriteLine("Cevap Kesinlikle " + cevap);
+                    else if (telefonCevap == 1)// TODO: Şıklar rastgele olacak. 1Doğru 1Yanlış cevap şeklinde
+                        Console.WriteLine("Emin Değilim. Ancak A veya B olduğunu düşünüyorum");
+                    else if (telefonCevap == 2)
+                        Console.WriteLine("Bilemedim. Kusura bakma");
+                    telefonHak = false;
+                }
+                else
+                    Console.WriteLine("Bu Joker Daha Önce Kullanıldı..");
+
+                goto yanitNoktasi;
+            }
             #endregion
 
         }
