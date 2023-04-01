df331d96b2538f0b9ba57943d92c92c39dc26fde
GulsumAYDOGDUERDIM <gulsumaydogduerdim@gmail.com>
2023-04-01 17:58:20 +0300

    feat: rules to the add

--- Program.cs
+++ Program.cs
@@ -6,10 +6,8 @@
         {
             #region Kim Milyoner Olmak İster - V1
             Console.WriteLine("Kim Milyoner Olmak İster\n");
-            Console.Write("İsim Giriniz: ");
+            Console.Write("İsim ve soyisim Giriniz: ");
             string isim = Console.ReadLine();
-            Console.Write("Soyisim Giriniz: ");
-            string soyisim = Console.ReadLine();
 
 
             Console.Write("\n1.Telefon Jokerinizi Giriniz: ");
@@ -28,7 +26,11 @@
             if (kuralCevap != 'E')
             {
                 // TODO: Kurallar eklenecek.
-                Console.WriteLine("Kurallar: asdfasdaads");
+                Console.WriteLine("Kurallar: "); 
+                Console.WriteLine("seyircilerle konuşmak yasaktır.");
+                Console.WriteLine("sarular dışarıdan destek alınmadan cevaplanmalıdır");
+                Console.WriteLine("telefon kullanmak yasaktır");
+
             }
 
             Console.Write("Hazır Mısınız? : ");
